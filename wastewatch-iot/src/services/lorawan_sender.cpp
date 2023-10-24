55681793b80d97fe077bfdaa1ae0206c5fffff49
Cristiano <cristianosampaio16@gmail.com>
2023-10-24 00:43:23 -0300

    feat: start LoRaWAN implementation

--- wastewatch-iot/src/services/lorawan_sender.cpp
+++ wastewatch-iot/src/services/lorawan_sender.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
-
 #include "lorawan_sender.h"
 
-void LoRaWANDataSender::sendData(const std::string& data) {
-    //TODO: use some lorawan lib to implement this
-    std::cout << "Sending data through LoRaWAN: " << data << std::endl;
+void LoRaWANDataSender::LoRaWANDataSender()
+{
+    // Startup serial
+    // TODO: Add constantes for this values
+    Serial.begin(9600);
+    delay(500);
+
+    // Startup all pins and UART
+    e32ttl.begin();
+    // TODO: Replace to the logger
+    Serial.println("Start LoRaWAN sender...");
+}
+
+void LoRaWANDataSender::sendData(const std::string &data)
+{
+    // Send message
+    ResponseStatus response = e32ttl.sendMessage(data);
+    // Check If there is some problem of succesfully send
+    // TODO: Replace to the logger
+    Serial.println("Data sending status: " + response.getResponseDescription() + "\n");
 }
--- wastewatch-iot/src/services/lorawan_sender.h
+++ wastewatch-iot/src/services/lorawan_sender.h
@@ -1,11 +1,23 @@
 #ifndef LORAWAN_SENDER_H
 #define LORAWAN_SENDER_H
 
+#include <iostream>
+
+#include "Arduino.h"
+#include "../../libraries/LoRa_E32/LoRa_E32.h"
+
 #include "sender_strategy.h"
 
-class LoRaWANDataSender : public DataSenderStrategy {
+class LoRaWANDataSender : public DataSenderStrategy
+{
 public:
-    void sendData(const std::string& data) override;
+    LoRaWANDataSender();
+    void sendData(const std::string &data) override;
+
+private:
+    // ---------- esp32 pins --------------
+    // TODO: Add constants for the asp pins
+    LoRa_E32 e32ttl(&Serial2, 15, 21, 19); //  RX AUX M0 M1
 };
 
 #endif
